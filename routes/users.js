258dfd4ae7159633b50aecbb60e69287ca15fe0c
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2021-08-22 00:09:29 +0900

    wip:パスワードの照合

--- routes/users.js
+++ routes/users.js
@@ -31,53 +31,49 @@ router.get('/signup', (req, res, next) => {
     res.render('users/signup', data);
 });
 
-router.post('/signup', (req, res, next) => {
-    async function checkpw() {
-        const hashed_pass = await hash_pass(req.body.pass, 10);
-        const hashed_pass2 = await hash_pass(req.body.pass2, 10);
-        console.log(hashed_pass);
-        console.log(hashed_pass2);
+router.post('/signup', async (req, res, next) => {
+    const pass = req.body.pass;
+    const pass2 = req.body.pass2;
+    const salt = 10;
+    if (pass === pass2) {
+        const hashed = await bcrypt.hash(pass, salt);
+        const form = {
+            name:   req.body.name,
+            age:    req.body.age,
+            mail:   req.body.mail,
+            pass:   hashed,
+            pass2:  hashed,
+            color:  req.body.color,
+            secret: req.body.secret
+        };
+    } else {
+        const form = {
+            name:   req.body.name,
+            age:    req.body.age,
+            mail:   req.body.mail,
+            pass:   req.body.pass,
+            pass2:  req.body.pass2,
+            color:  req.body.color,
+            secret: req.body.secret
+        };
     }
+    db.sequelize.sync()
+        .then(() => db.User.create(form))
+        .then(usr => {
+            res.redirect('/users/login');
+        })
+        .catch(err => {
+            let data = {
+                title: 'SIGN UP',
+                content: '以下のフォームを入力して新規登録してください。',
+                form: form,
+                err: err
+            }
+            console.log(err);
+            res.render('users/signup', data);
+        });
 
-    async function hash_pass(password, stretch) {
-        let hashedPassword = await bcrypt.hash(password, stretch);
-        return hashedPassword;
-    }
-
-    const form = {
-        name:   req.body.name,
-        age:    req.body.age,
-        mail:   req.body.mail,
-        pass:   req.body.pass,
-        pass2:  req.body.pass2,
-        color:  req.body.color,
-        secret: req.body.secret
-    };
-    let err;
-
-    let data = {
-        title: 'SIGN UP',
-        content: '以下のフォームを入力して新規登録してください。',
-        form: form,
-        err: err
-    }
-    res.render('user/signup', data);
     
-    // db.sequelize.sync()
-    //     .then(() => db.User.create(form))
-    //     .then(usr => {
-    //         res.redirect('/users/login');
-    //     })
-    //     .catch(err => {
-    //         let data = {
-    //             title: 'SIGN UP',
-    //             content: '以下のフォームを入力して新規登録してください。',
-    //             form: form,
-    //             err: err
-    //         }
-    //         console.log(err);
-    //         res.render('users/signup', data);
-    //     });
 });
 
 module.exports = router;
